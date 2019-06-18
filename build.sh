5aaf520ca6f4b7f343bdbea9f8485526fc80c675
Dmytro Mishkin <ducha.aiki@gmail.com>
2019-06-18 11:45:01 +0200

    working, but with hardcodes

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "Configuring and building Thirdparty/DBoW2 ..."
 cd Thirdparty/DBoW2
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DEIGEN3_INCLUDE_DIR=/home/old-ufo/dev/navigation-benchmark/3rdparty/eigen3_installed/include/eigen3/ -DCMAKE_INSTALL_PREFIX=/home/old-ufo/dev/navigation-benchmark/3rdparty/ORBSLAM2_installed 
 make -j
 
 cd ../../g2o
@@ -12,7 +12,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DEIGEN3_INCLUDE_DIR=/home/old-ufo/dev/navigation-benchmark/3rdparty/eigen3_installed/include/eigen3/ -DCMAKE_INSTALL_PREFIX=/home/old-ufo/dev/navigation-benchmark/3rdparty/ORBSLAM2_installed 
 make -j
 
 cd ../../../
@@ -27,5 +27,5 @@ echo "Configuring and building ORB_SLAM2 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DSaddle_DIR=/home/old-ufo/dev/navigation-benchmark/3rdparty/Saddle_SLAM/Thirdparty/saddle/src/ -DCMAKE_BUILD_TYPE=Release -DEIGEN3_INCLUDE_DIR=/home/old-ufo/dev/navigation-benchmark/3rdparty/eigen3_installed/include/eigen3/ -DCMAKE_INSTALL_PREFIX=/home/old-ufo/dev/navigation-benchmark/3rdparty/ORBSLAM2_installed 
 make -j
